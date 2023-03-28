c1383fef211f9fc5ecae709f811191ba9326adbd
Lux Ilya <iliya92@mail.ru>
2023-03-28 13:20:27 +0300

    lec3 added

---
+++ lec3/task1.py
@@ -0,0 +1,8 @@
+def summ_num(n):
+    summa = 0
+    for i in range(1, n+1):
+        summa += i
+    return summa
+
+a = summ_num(5)
+print(a)
---
+++ lec3/task3.py
@@ -0,0 +1,5 @@
+# from modul1 import max1 # импорт одной функции
+# from modul1 import * # импорт всех функций в файле
+import modul1 as m1 # назначить имя файла для удобства
+
+print(m1.max1(15,9))
