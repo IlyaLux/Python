b9f78468462e721c1e6224e3798a809a27839122
Lux Ilya <iliya92@mail.ru>
2023-04-02 14:51:58 +0300

    lec4 fixes

--- lec4/zip.py
+++ lec4/zip.py
@@ -2,4 +2,4 @@ users = ['user1', 'user2', 'user3', 'user4', 'user5']
 ids = [4, 5, 9, 14, 7]
 salary = [111, 222, 333]
 data = list(zip(users, ids, salary))
-print(data)
+print(data) # [('user1', 4, 111), ('user2', 5, 222), ('user3', 9, 333)]
